0588a8a05c4d90c71228aa71135ebe961f9f4d57
Loganathan Sellappa <loganathan.ms@gmail.com>
2021-11-27 23:21:06 +0100

    GreedyFlorist

--- greedy/greedyFlorist.js
+++ greedy/greedyFlorist.js
@@ -21,3 +21,20 @@ function getMinimumCost(peopleCount, cost) {
         return sum;
     }
 }
+
+
+
+// Optimal approach O(N log N)
+// Complete the getMinimumCost function below.
+function getMinimumCost(peopleCount, cost) {
+    if(peopleCount === cost.length) {
+        return cost.reduce((pv, cv) => pv+cv, 0)
+    } else {
+        let sum = 0
+        const sortedFlowers = cost.sort((a, b) => (a > b ? -1 : 1))
+         for (let i = 0; i < sortedFlowers.length; i++) {
+            sum += (Math.floor(i/peopleCount) +1) * sortedFlowers[i];
+        };
+       return sum;
+    }
+}
